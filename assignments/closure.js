230f0b764655a318d2fd5c46fbafd33f752006a1
Anthony Lopez <anthonybeconscious@gmail.com>
2019-10-15 13:09:04 -0500

    started work on closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,13 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+let something = "this";
+function closure(){
+  
+  let somethingElse = `${something} is a closure`;
+  return somethingElse;
+};
+console.log(closure());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
